58ef19296c26f0b88d8dad8e4ea724e520d4b662
Zameer Hombali <zameerhombali01@gmail.com>
2025-09-30 08:28:09 +0530

    ssl file

--- config.php
+++ config.php
@@ -19,31 +19,31 @@
 <?php
 // Show all errors
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
-$host = "cloud-database-db.mysql.database.azure.com";
-$user = "zameer";  // must include @servername
-$password = "ZAIDISGAY*123";
-$dbname = "attendances_db";
-$port = 3306;
+// $host = "cloud-database-db.mysql.database.azure.com";
+// $user = "zameer";  // must include @servername
+// $password = "ZAIDISGAY*123";
+// $dbname = "attendances_db";
+// $port = 3306;
 
-// Path to SSL certificate required by Azure MySQL
-$ssl_ca = __DIR__ . "/DigiCertGlobalRootG2.crt.pem";
+// // Path to SSL certificate required by Azure MySQL
+// $ssl_ca = __DIR__ . "/DigiCertGlobalRootG2.crt.pem";
 
-// Start mysqli connection
-$conn = mysqli_init();
+// // Start mysqli connection
+// $conn = mysqli_init();
 
-// Tell MySQLi to use SSL
-mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
+// // Tell MySQLi to use SSL
+// mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
 
-// Try to connect
-if (!mysqli_real_connect($conn, $host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
-    die("❌ Connection failed: " . mysqli_connect_error());
-} else {
-    echo "✅ Connected successfully to Azure MySQL!";
-}
+// // Try to connect
+// if (!mysqli_real_connect($conn, $host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+//     die("❌ Connection failed: " . mysqli_connect_error());
+// } else {
+//     echo "✅ Connected successfully to Azure MySQL!";
+// }
 ?>
 <?php
 // =========================
