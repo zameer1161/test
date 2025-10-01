75f05853e387f34b532385948570c59040ddd243
Zameer Hombali <zameerhombali01@gmail.com>
2025-10-01 09:39:27 +0530

    update

--- config.php
+++ config.php
@@ -1,19 +1,3 @@
-<?php
-
-// $host = "localhost";   // Database host
-// $db   = "attendance_db"; // Database name
-// $user = "root";        // Database username
-// $pass = "";            // Database password
-
-// try {
-//     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
-//     // Set common PDO options
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-// } catch (PDOException $e) {
-//     die("Database Connection Failed: " . $e->getMessage());
-// }
-?>
 
 <?php
 $host = "cloud-database-db.mysql.database.azure.com";
